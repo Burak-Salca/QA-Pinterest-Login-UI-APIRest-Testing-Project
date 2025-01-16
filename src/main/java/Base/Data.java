e9758bceb29e2feba6e400d7bf7f81c411feabc4
Burak-Salca <salcaburak@gmail.com>
2025-01-16 22:06:20 +0300

    Code trimming2

---
+++ src/main/java/Base/Data.java
@@ -0,0 +1,7 @@
+package Base;
+
+public class Data {
+    public String url = "https://tr.pinterest.com/login/";
+    public String email = "siyah.0699@hotmail.com";
+    public String password = "Siyah.0699";
+}
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,3 +1,4 @@
+import Base.BaseTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,33 +9,20 @@ import org.testng.annotations.Test;
 
 import java.util.regex.Pattern;
 
-public class LoginTests {
-
-    WebDriver driver;
-
-    @BeforeMethod
-    public void openBrowser(){
-
-        driver = new ChromeDriver();
-        driver.get("https://tr.pinterest.com/login/");
-
-    }
+public class LoginTests extends BaseTest {
 
     @Test
     public void TC01() throws InterruptedException{
-
-        driver.findElement(By.cssSelector("input#email")).sendKeys("siyah.0699@hotmail.com");
-        driver.findElement(By.cssSelector("input#password")).sendKeys("Siyah.0699");
+        driver.findElement(By.cssSelector("input#email")).sendKeys(email);
+        driver.findElement(By.cssSelector("input#password")).sendKeys(password);
         driver.findElement(By.cssSelector("[class=\"RCK Hsu USg adn gn8 L4E kVc iyn S9z Vxj aZc pXK _co Il7 hNT BG7\"]")).click();
         Thread.sleep(5000);
         System.out.println(driver.findElement(By.cssSelector("[class=\"sGz dyH iFc H2s\"]")).getText());
         Assert.assertEquals(driver.findElement(By.cssSelector("[class=\"sGz dyH iFc H2s\"]")).getText(),"S");
-
     }
 
     @Test
     public void TC02() throws InterruptedException{
-
         driver.findElement(By.cssSelector("input#email")).sendKeys("siyah.0699qhotmail.com");
         driver.findElement(By.cssSelector("input#password")).sendKeys("Siyah.0699");
         driver.findElement(By.cssSelector("[class=\"RCK Hsu USg adn gn8 L4E kVc iyn S9z Vxj aZc pXK _co Il7 hNT BG7\"]")).click();
@@ -49,30 +37,21 @@ public class LoginTests {
 
     @Test
     public void TC03() throws InterruptedException{
-
         driver.findElement(By.cssSelector("input#email")).sendKeys("siyah.9999@hotmail.com");
         driver.findElement(By.cssSelector("input#password")).sendKeys("Siyah.0699");
         driver.findElement(By.cssSelector("[class=\"RCK Hsu USg adn gn8 L4E kVc iyn S9z Vxj aZc pXK _co Il7 hNT BG7\"]")).click();
         Thread.sleep(5000);
         System.out.println(driver.findElement(By.xpath("//*[@id=\"email-error\"]/div/div/div[2]")).getText());
         Assert.assertEquals(driver.findElement(By.xpath("//*[@id=\"email-error\"]/div/div/div[2]")).getText().trim(),"Girdiğiniz e-posta hiçbir hesaba ait değil.".trim());
-
     }
 
     @Test
     public void TC04() throws InterruptedException{
-
         driver.findElement(By.cssSelector("input#email")).sendKeys("siyah.0699@hotmail.com");
         driver.findElement(By.cssSelector("input#password")).sendKeys("000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000");
         driver.findElement(By.cssSelector("[class=\"RCK Hsu USg adn gn8 L4E kVc iyn S9z Vxj aZc pXK _co Il7 hNT BG7\"]")).click();
         Thread.sleep(5000);
         System.out.println(driver.findElement(By.xpath("//*[@id=\"password-error\"]/div/div/div[2]/div/span[1]")).getText());
         Assert.assertEquals(driver.findElement(By.xpath("//*[@id=\"password-error\"]/div/div/div[2]/div/span[1]")).getText().trim(),"Girdiğiniz parola yanlış. Tekrar deneyin veya ".trim());
-
-    }
-
-    @AfterMethod
-    public void closeBrowser(){
-        driver.quit();
     }
 }
