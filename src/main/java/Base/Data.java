3c5a3467fd6b21d7c5ce39a4c92a1dc0b10f1bc1
Burak-Salca <salcaburak@gmail.com>
2025-01-21 05:21:56 +0300

    data fixed

--- src/main/java/Base/Data.java
+++ src/main/java/Base/Data.java
@@ -3,6 +3,6 @@ package Base;
 public class Data {
     public String url = "https://tr.pinterest.com/login/";
     public String email = "siyah.0699@hotmail.com";
-    public String password = "Siyah.0699";
+    public String password = "siyah.0699";
     public String emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
 }
